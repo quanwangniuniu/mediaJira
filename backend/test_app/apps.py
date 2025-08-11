198afbd2d7eb892c481d7c9f4b47b6c6a3c7d168
Ray <119475239+quanwangniuniu@users.noreply.github.com>
2025-08-11 13:46:28 +1000

    Delete backend/test_app directory

--- backend/test_app/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-import os
-
-
-class TestAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'test_app'
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__))) 
