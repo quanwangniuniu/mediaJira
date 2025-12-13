0ed8a6b06f3770a71a2d422052ce4749003d93b0
Jamie <2993429266@qq.com>
2025-12-13 11:28:00 +1100

    Replace sqlite3 with postgres

--- backend/backend/ci_settings.py
+++ backend/backend/ci_settings.py
@@ -1,9 +1,15 @@
-
+import os
 from .settings import *
 
+# Using postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',  
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
+
