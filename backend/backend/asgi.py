3b763191f215a93b4c185c425fff57bab1a8f694
Jamie <2993429266@qq.com>
2025-12-16 22:43:44 +1100

    Fix Jaeger issues

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -7,29 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-# import os
-# import django
-# from django.core.asgi import get_asgi_application
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.auth import AuthMiddlewareStack
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-# django.setup()
-
-# from asset.routing import websocket_urlpatterns as asset_websocket_urlpatterns
-# from campaign.routing import websocket_urlpatterns as campaign_websocket_urlpatterns
-# from asset.middleware import JWTAuthMiddleware
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": JWTAuthMiddleware(
-#         URLRouter(
-#             asset_websocket_urlpatterns + campaign_websocket_urlpatterns
-#         )
-#     ),
-# })
-
-
 import os
 import django
 from django.core.asgi import get_asgi_application
