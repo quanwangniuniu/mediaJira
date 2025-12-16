b6ec6798bb60f8376d3453e2f3e6771ae6731932
Jamie <2993429266@qq.com>
2025-12-16 22:41:17 +1100

    Fix Jaeger issues

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -7,11 +7,35 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
+# import os
+# import django
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# django.setup()
+
+# from asset.routing import websocket_urlpatterns as asset_websocket_urlpatterns
+# from campaign.routing import websocket_urlpatterns as campaign_websocket_urlpatterns
+# from asset.middleware import JWTAuthMiddleware
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": JWTAuthMiddleware(
+#         URLRouter(
+#             asset_websocket_urlpatterns + campaign_websocket_urlpatterns
+#         )
+#     ),
+# })
+
+
 import os
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
@@ -20,8 +44,12 @@ from asset.routing import websocket_urlpatterns as asset_websocket_urlpatterns
 from campaign.routing import websocket_urlpatterns as campaign_websocket_urlpatterns
 from asset.middleware import JWTAuthMiddleware
 
+
+http_application = get_asgi_application()
+http_application = OpenTelemetryMiddleware(http_application)
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": http_application,
     "websocket": JWTAuthMiddleware(
         URLRouter(
             asset_websocket_urlpatterns + campaign_websocket_urlpatterns
